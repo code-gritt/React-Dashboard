7e488afe10d5acee987bcda9902f214dd0db24f2
Gokul <gokulchandan24@gmail.com>
2024-06-12 00:02:40 +0530

    app finishing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
 import Topbar from "./components/topbar/Topbar";
 import Sidebar from "./components/sidebar/Sidebar";
 import "./App.css";
 import Homepage from "./pages/homepage/Homepage";
+import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
+import NewUser from "./pages/newUser/NewUser";
+import ProductList from "./pages/productList/ProductList";
+import Product from "./pages/product/Product";
+import NewProduct from "./pages/newProduct/NewProduct";
 
 function App() {
   return (
@@ -12,11 +18,17 @@ function App() {
         <div className="container">
           <Sidebar />
           <div className="others">
-            <Routes>
+            <Switch>
               <Route>
                 <Route path="/" element={<Homepage />} />
               </Route>
-            </Routes>
+              <Route path="/users" element={<UserList />} />
+              <Route path="/user/:userId" element={<User />} />
+              <Route path="/newUser" element={<NewUser />} />
+              <Route path="/products" element={<ProductList />} />
+              <Route path="/product/:productId" element={<Product />} />
+              <Route path="/newproduct" element={<NewProduct />} />
+            </Switch>
           </div>
         </div>
       </BrowserRouter>
--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,19 +1,23 @@
+import { userData } from "../../../dummyData";
+import Chart from "../../components/chart/Chart";
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
+import WidgetLg from "../../components/widgetLg/WidgetLg";
+import WidgetSm from "../../components/widgetSm/WidgetSm";
 import "./home.css";
 
 export default function Homepage() {
   return (
     <div className="home">
       <FeaturedInfo />
-      {/* <Chart
+      <Chart
         data={userData}
         title="User Analytics"
         grid
         dataKey="Active User"
-      /> */}
+      />
       <div className="homeWidgets">
-        {/* <WidgetSm />
-        <WidgetLg /> */}
+        <WidgetSm />
+        <WidgetLg />
       </div>
     </div>
   );
